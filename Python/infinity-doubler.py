7f6f9bcfb1b8893b6a913d2c1a20cb3fe3229123
Sipho <62767262+Siphocha@users.noreply.github.com>
2023-10-11 11:16:41 +0200

    first push for hacktoberfest! great stuff

---
+++ Python/infinity-doubler.py
@@ -0,0 +1,10 @@
+import random, time
+
+#This program doubles the numbers infinitely
+#First Hacktoberfest commit wooooooo!
+
+i = 1
+while i >= 1:
+    i = i * 2
+    print("{}". format(i))
+    time.sleep(1)
